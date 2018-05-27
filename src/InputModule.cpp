f373da31b994f4692ee5924e062001580d5d73fe
Vincius Pinto <viniciuspintofp@gmail.com>
2018-05-27 16:07:12 -0300

    Fixes InputModule warnings Improves Makefile output appearance

--- src/InputModule.cpp
+++ src/InputModule.cpp
@@ -76,9 +76,9 @@ void InputModule::Update()
 	// Reset joystick buttons
 	if (_joysticksInit)
 	{
-		for (int i = 0; i < _joyButtonStates.size(); i++)
+		for (unsigned int i = 0; i < _joyButtonStates.size(); i++)
 		{
-			for (int j = 0; j < _joyButtonStates[i].size(); j++)
+			for (unsigned int j = 0; j < _joyButtonStates[i].size(); j++)
 			{
 				_joyButtonStates[i][j].isUp = false;
 				_joyButtonStates[i][j].isDown = false;
@@ -249,7 +249,7 @@ void InputModule::Update()
 void InputModule::Clean()
 {
 	if (_joysticksInit)
-		for (unsigned int i = 0; i < SDL_NumJoysticks(); i++)
+		for (int i = 0; i < SDL_NumJoysticks(); i++)
 		{
 			SDL_JoystickClose(_joysticks[i]);
 		}
